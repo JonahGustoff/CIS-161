5a06e4333e65a69432b952e7546ba253c79b2e4d
JonahGustoff <46538867+JonahGustoff@users.noreply.github.com>
2019-01-16 11:09:05 -0600

    Add files via upload

---
+++ datatypesizegustoff.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+using namespace std;
+
+ int main() 
+ {
+	 cout << "Size of Charater = " << sizeof(char) << endl;
+	 cout << "Size of Interger = " << sizeof(int) << endl;
+	 cout << "Size of Float = " << sizeof(float) << endl;
+	 cout << "Size of Double = " << sizeof(double) << endl;
+
+	 system("pause");
+	return 0;
+}
